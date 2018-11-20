b6aac52d5ed7515e9d6d148f9bdc31bb515cb9bb
SkipCat <cecilehuguet@laposte.net>
2018-11-20 12:26:43 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <NavBar />
+    <main class="container">
+      <router-view/>
+    </main>
   </div>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 #app {
@@ -16,16 +26,32 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
+
+/* #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
+} */
+
+.navbar-nav.nav-right {
+  margin-left: auto;
+}
+
+main.container {
+  margin-top: 2rem;
+}
+
+.search-results {
+  flex-wrap: wrap;
+}
+
+.search-results .card {
+  min-width: 200px;
+  max-width: 300px;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>  
+  <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+    <router-link to="/" class="navbar-brand">
+      <i class="fa fa-headphones"></i>
+      Deezweb
+    </router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+        aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav nav-right">
+        <router-link to="/search" class="nav-item nav-link active">
+          <i class="fa fa-search"></i> 
+          Recherche
+        </router-link>
+        <router-link to="#" class="nav-item nav-link">
+          <i class="fa fa-heart"></i> 
+          Mes favoris
+        </router-link>
+        <router-link to="/login" class="nav-item nav-link">
+          <i class="fa fa-sign-in"></i> 
+          Connexion
+        </router-link>
+        <router-link to="/register" class="nav-item nav-link">
+          <i class="fa fa-user-plus"></i> 
+          Inscription
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
